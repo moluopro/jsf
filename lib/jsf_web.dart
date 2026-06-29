70f146cc4a1189102bb723857687e0b450c7a58c
moluopro <moluopro@qq.com>
2026-06-29 14:51:53 +0800

    feat: add web plugin registrar

---
+++ lib/jsf_web.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// Web registrar used by Flutter's generated plugin registrant.
+///
+/// JSF's web runtime is implemented with conditional exports from
+/// `package:jsf/jsf.dart`, so registration does not need to install method
+/// channels or global state.
+class JsfWeb {
+  /// Registers the web plugin with Flutter.
+  static void registerWith(Registrar registrar) {}
+}
