8afcb92e23f4a766478cc8017130632b07afc083
moluopro <moluopro@qq.com>
2026-06-29 14:51:54 +0800

    build: forward iOS bridge source to shared native code

--- ios/Classes/jsf.c
+++ ios/Classes/jsf.c
@@ -1,3 +1,2 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../jsf.podspec for more information.
+// Forwarder used by CocoaPods so native sources stay shared with CMake builds.
 #include "../../src/jsf.c"
